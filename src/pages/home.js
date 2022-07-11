10319424bef2a2b63a3745abe2dfd70af7009402
macyso12 <macyso12@gmail.com>
2022-07-11 09:58:29 -0400

    update stlying

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,7 +17,7 @@ function Home() {
                 border: "2px solid #57d655",
                 padding: "2.5%",
                 color: "#57d655",
-                borderRadius: "20%",
+                borderRadius: "25%",
                 textDecoration: "none",
               }}
               href="try-it-yourself"
