12fdad45f49e0d2dd1aa4840981d55b14307120f
Akosah <emmanuel.s.akosah.19@gmail.com>
2023-10-10 16:48:30 -0400

    added all tasks

--- src/data.js
+++ src/data.js
@@ -16,129 +16,129 @@ export const camMicCheckTask = {
   video: "#",
 };
 export const data = [
-  // {
-  //   id: "0",
-  //   category: "",
-  //   title: "Practice Task: Model Fractions",
-  //   preview:
-  //     'Click "start" to begin a brief, required tutorial about this site.',
-  //   previewImage: TutorialPreviewImage,
-  //   description: [
-  //     "You are introducing models of fractions for your 3rd grade class. You have modeled one-half, now you are moving to fourths.",
-  //     "You are going to model one-fourth with a rectangle",
-  //   ],
-  //   problem: ["You are going to model one-fourth with a rectangle"],
-  //   task: [
-  //     "Model how to represent one-fourth with this rectangular shape.",
-  //     " Use concise and precise mathematical language.",
-  //   ],
-  //   prompts: [
-  //     "Model how to represent one-fourth with a rectangle.",
-  //     "Use concise and precise mathematical language.",
-  //   ],
-  //   time: "5 mins",
-  //   imgURL: "",
-  //   video: "#",
-  // },
-  // {
-  //   id: "1",
-  //   category: "math",
-  //   title: "Task 1.1 Think Aloud",
-  //   preview:
-  //     'Click "start" to begin your this task. Second grade math context.',
-  //   previewImage: TaskPreviewImage,
-  //   description: [
-  //     "You are working with your second graders on word problems.",
-  //     "Your students can solve addition and subtraction problems, but the complexity of word problems can challenge your students.",
-  //     "You are going to provide a think aloud to make sense of word problems In your think aloud you want to address how you make sense of the context, quantities, mathematical relationships in the problem and how you can use a visual to represent what is happening in the problem.",
-  //   ],
-  //   problem:
-  //     "Ava’s mom is 28 years old. Her grandma is 51. How much older is Ava’s grandma than her mom?",
+  {
+    id: "0",
+    category: "",
+    title: "Practice Task: Model Fractions",
+    preview:
+      'Click "start" to begin a brief, required tutorial about this site.',
+    previewImage: TutorialPreviewImage,
+    description: [
+      "You are introducing models of fractions for your 3rd grade class. You have modeled one-half, now you are moving to fourths.",
+      "You are going to model one-fourth with a rectangle",
+    ],
+    problem: ["You are going to model one-fourth with a rectangle"],
+    task: [
+      "Model how to represent one-fourth with this rectangular shape.",
+      " Use concise and precise mathematical language.",
+    ],
+    prompts: [
+      "Model how to represent one-fourth with a rectangle.",
+      "Use concise and precise mathematical language.",
+    ],
+    time: "5 mins",
+    imgURL: "",
+    video: "#",
+  },
+  {
+    id: "1",
+    category: "math",
+    title: "Task 1.1 Think Aloud",
+    preview:
+      'Click "start" to begin your this task. Second grade math context.',
+    previewImage: TaskPreviewImage,
+    description: [
+      "You are working with your second graders on word problems.",
+      "Your students can solve addition and subtraction problems, but the complexity of word problems can challenge your students.",
+      "You are going to provide a think aloud to make sense of word problems In your think aloud you want to address how you make sense of the context, quantities, mathematical relationships in the problem and how you can use a visual to represent what is happening in the problem.",
+    ],
+    problem:
+      "Ava’s mom is 28 years old. Her grandma is 51. How much older is Ava’s grandma than her mom?",
 
-  //   task: ["Provide a think aloud to make sense of word problems."],
-  //   prompts: [
-  //     "Provide a think aloud to make sense of word problems.",
-  //     "Share the purpose for your think aloud with your students.",
+    task: ["Provide a think aloud to make sense of word problems."],
+    prompts: [
+      "Provide a think aloud to make sense of word problems.",
+      "Share the purpose for your think aloud with your students.",
 
-  //     "Narrate your thinking about how you approach understanding this problem.",
+      "Narrate your thinking about how you approach understanding this problem.",
 
-  //     "Address how you make sense of the context, quantities, and mathematical relationships.",
+      "Address how you make sense of the context, quantities, and mathematical relationships.",
 
-  //     "Demonstrate how you might represent the context of the problem with manipulatives, a diagram or other visual.",
+      "Demonstrate how you might represent the context of the problem with manipulatives, a diagram or other visual.",
 
-  //     " Ensure that you are modeling accuracy, clarity, and precision to mathematical language.",
-  //   ],
-  //   time: "5 mins",
-  //   imgURL: "",
-  //   video: "https://www.youtube.com/watch?v=PV-W5iG6yXQ",
-  // },
-  // {
-  //   id: "2",
-  //   category: "english",
-  //   title: "Task 1.2 Think Aloud",
-  //   preview:
-  //     'Click "start" to begin your this task. Fourth grade math context.',
-  //   previewImage: TaskPreviewImage,
-  //   description: [
-  //     "*Note: This task stands alone. Do not assume your audience has knowledge of Task 1.1.",
+      " Ensure that you are modeling accuracy, clarity, and precision to mathematical language.",
+    ],
+    time: "5 mins",
+    imgURL: "",
+    video: "https://www.youtube.com/watch?v=PV-W5iG6yXQ",
+  },
+  {
+    id: "2",
+    category: "english",
+    title: "Task 1.2 Think Aloud",
+    preview:
+      'Click "start" to begin your this task. Fourth grade math context.',
+    previewImage: TaskPreviewImage,
+    description: [
+      "*Note: This task stands alone. Do not assume your audience has knowledge of Task 1.1.",
 
-  //     "You are working with your fourth-grade class on word problems.",
+      "You are working with your fourth-grade class on word problems.",
 
-  //     "Your students can solve addition and subtraction problems, but the complexity of word problems can challenge your students. They are also familiar with adding and subtracting fractions with like denominators.",
+      "Your students can solve addition and subtraction problems, but the complexity of word problems can challenge your students. They are also familiar with adding and subtracting fractions with like denominators.",
 
-  //     "You are going to provide a think aloud to make sense of word problems. In your think aloud you want to address how you make sense of the context, quantities, mathematical relationships in the problem and how you can use a visual to represent what is happening in the problem.",
-  //   ],
-  //   task: ["Provide a think aloud to make sense of word problems."],
-  //   problem:
-  //     "CeCe is making cookies with white and brown sugar. She puts 2/8 cup of white sugar in the cookie dough. Then she puts in the brown sugar. She has 7/8 cup of sugar in all. How much brown sugar is in the recipe?",
-  //   prompts: [
-  //     "Share the purpose for your think aloud with your students.",
+      "You are going to provide a think aloud to make sense of word problems. In your think aloud you want to address how you make sense of the context, quantities, mathematical relationships in the problem and how you can use a visual to represent what is happening in the problem.",
+    ],
+    task: ["Provide a think aloud to make sense of word problems."],
+    problem:
+      "CeCe is making cookies with white and brown sugar. She puts 2/8 cup of white sugar in the cookie dough. Then she puts in the brown sugar. She has 7/8 cup of sugar in all. How much brown sugar is in the recipe?",
+    prompts: [
+      "Share the purpose for your think aloud with your students.",
 
-  //     "Narrate your thinking about how you approach understanding this problem.",
+      "Narrate your thinking about how you approach understanding this problem.",
 
-  //     "Address how you make sense of the context, quantities, and mathematical relationships.",
+      "Address how you make sense of the context, quantities, and mathematical relationships.",
 
-  //     "Demonstrate how you might represent the context of the problem with manipulatives, a diagram or other visual.",
+      "Demonstrate how you might represent the context of the problem with manipulatives, a diagram or other visual.",
 
-  //     "Ensure that you are modeling accuracy, clarity, and precision to mathematical language",
-  //   ],
-  //   time: "5 mins",
-  //   imgURL: "",
-  //   video: "#",
-  // },
-  // {
-  //   id: "3",
-  //   category: "english",
-  //   title: "Task 1.3 Think Aloud",
-  //   preview: 'Click "start" to begin your this task. Fifth grade math context.',
-  //   previewImage: TaskPreviewImage,
-  //   description: [
-  //     "*Note: This task stands alone. Do not assume your audience has knowledge of Task 1.1 or Task 1.2.",
+      "Ensure that you are modeling accuracy, clarity, and precision to mathematical language",
+    ],
+    time: "5 mins",
+    imgURL: "",
+    video: "#",
+  },
+  {
+    id: "3",
+    category: "english",
+    title: "Task 1.3 Think Aloud",
+    preview: 'Click "start" to begin your this task. Fifth grade math context.',
+    previewImage: TaskPreviewImage,
+    description: [
+      "*Note: This task stands alone. Do not assume your audience has knowledge of Task 1.1 or Task 1.2.",
 
-  //     "You are working with your fifth-grade students on estimating the answer in math problems.",
+      "You are working with your fifth-grade students on estimating the answer in math problems.",
 
-  //     "They are familiar with rounding to the nearest tens or hundreds place, and using tables. The challenge for students is when all of those skills are combined into a more complex problem. You are going to provide a think aloud to show how you make sense of problems that combine multiple skills.",
-  //   ],
-  //   problem:
-  //     "About how much would it cost to buy MP3 players for all 18 classrooms in your school? Use the table to answer the question.",
-  //   task: [
-  //     "Provide a think aloud to make sense of word problems like this one.",
-  //   ],
-  //   prompts: [
-  //     "Share the purpose for your think aloud with your students.",
+      "They are familiar with rounding to the nearest tens or hundreds place, and using tables. The challenge for students is when all of those skills are combined into a more complex problem. You are going to provide a think aloud to show how you make sense of problems that combine multiple skills.",
+    ],
+    problem:
+      "About how much would it cost to buy MP3 players for all 18 classrooms in your school? Use the table to answer the question.",
+    task: [
+      "Provide a think aloud to make sense of word problems like this one.",
+    ],
+    prompts: [
+      "Share the purpose for your think aloud with your students.",
 
-  //     "Narrate your thinking about how you approach understanding this problem.",
+      "Narrate your thinking about how you approach understanding this problem.",
 
-  //     "Address how you make sense of the context, quantities, and mathematical relationships.",
+      "Address how you make sense of the context, quantities, and mathematical relationships.",
 
-  //     "Demonstrate how you might represent the context of the problem with manipulatives, a diagram or other visual.",
+      "Demonstrate how you might represent the context of the problem with manipulatives, a diagram or other visual.",
 
-  //     "Ensure that you are modeling accuracy, clarity, and precision to mathematical language.",
-  //   ],
-  //   time: "5 mins",
-  //   imgURL: Table1,
-  //   video: "#",
-  // },
+      "Ensure that you are modeling accuracy, clarity, and precision to mathematical language.",
+    ],
+    time: "5 mins",
+    imgURL: Table1,
+    video: "#",
+  },
   {
     id: "4",
     category: "mathematics",
